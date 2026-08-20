4a474fb36b8f70220e4ca5cfb964a2b95289ba0a
hanyfreestyle <81318840+hanyfreestyle@users.noreply.github.com>
2026-08-20 16:16:54 +0300

    Re-enable farm question seeding through sync service

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,21 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder {
-  /**
-   * Seed the application's database.
-   */
-  public function run(): void {
-    $this->call([
-      RolesAndPermissionsSeeder::class,
-      AdminUserSeeder::class,
-      UploadFilterSeeder::class,
-      QuestionnaireSectionSeeder::class,
-
-      // Temporarily disabled while Farm Structure questions are being redesigned.
-      // QuestionnaireFarmQuestionsSeeder::class,
-    ]);
-  }
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call([
+            RolesAndPermissionsSeeder::class,
+            AdminUserSeeder::class,
+            UploadFilterSeeder::class,
+            QuestionnaireSectionSeeder::class,
+            QuestionnaireFarmQuestionsSeeder::class,
+        ]);
+    }
 }
