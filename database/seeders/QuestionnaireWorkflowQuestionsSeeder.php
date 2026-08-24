50032d2cfe9bdf5da9acf03e0fb3f26be945e714
hanyfreestyle <81318840+hanyfreestyle@users.noreply.github.com>
2026-08-25 02:04:42 +0300

    Register animal intake workflow questions seeder

--- database/seeders/QuestionnaireWorkflowQuestionsSeeder.php
+++ database/seeders/QuestionnaireWorkflowQuestionsSeeder.php
@@ -2,12 +2,15 @@
 
 namespace Database\Seeders;
 
+use Database\Seeders\Questions\Workflow\AnimalIntakeQuestionsSeeder;
 use Illuminate\Database\Seeder;
 
 class QuestionnaireWorkflowQuestionsSeeder extends Seeder
 {
     public function run(): void
     {
-        // Question seeders will be added here when section 4 question design begins.
+        $this->call([
+            AnimalIntakeQuestionsSeeder::class,
+        ]);
     }
 }
