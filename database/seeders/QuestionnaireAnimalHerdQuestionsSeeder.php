b61e2749270f2508565af79b7ac4e84ce86337e9
hanyfreestyle <81318840+hanyfreestyle@users.noreply.github.com>
2026-08-24 14:52:24 +0300

    Add animal herd question orchestrator

---
+++ database/seeders/QuestionnaireAnimalHerdQuestionsSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class QuestionnaireAnimalHerdQuestionsSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Question seeders will be added here when section 3 question design begins.
+    }
+}
