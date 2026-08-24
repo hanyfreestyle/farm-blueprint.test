2395304874800642514766b1d3df22208474ac4b
hanyfreestyle <81318840+hanyfreestyle@users.noreply.github.com>
2026-08-24 14:52:36 +0300

    Add workflow question orchestrator

---
+++ database/seeders/QuestionnaireWorkflowQuestionsSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class QuestionnaireWorkflowQuestionsSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Question seeders will be added here when section 4 question design begins.
+    }
+}
