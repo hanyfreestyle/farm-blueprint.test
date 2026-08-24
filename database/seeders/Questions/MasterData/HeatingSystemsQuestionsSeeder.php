1743613cb859744c5029da45c317742df080836e
hanyfreestyle <81318840+hanyfreestyle@users.noreply.github.com>
2026-08-24 12:46:05 +0300

    Move heating systems questions to master data namespace

---
+++ database/seeders/Questions/MasterData/HeatingSystemsQuestionsSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders\Questions\MasterData;
+
+use Database\Seeders\Questions\Concerns\ReferenceMasterDataQuestionsSeeder;
+
+class HeatingSystemsQuestionsSeeder extends ReferenceMasterDataQuestionsSeeder
+{
+    protected function sectionName(): string
+    {
+        return 'أنظمة التدفئة';
+    }
+
+    protected function singularLabel(): string
+    {
+        return 'نظام التدفئة';
+    }
+
+    protected function pluralLabel(): string
+    {
+        return 'أنظمة التدفئة';
+    }
+
+    protected function seedKeyPrefix(): string
+    {
+        return 'heating_system';
+    }
+
+    protected function targetEntity(): string
+    {
+        return 'heating_system';
+    }
+
+    protected function initialValues(): array
+    {
+        return [
+            ['label' => 'التدفئة الكهربائية', 'value' => 'electric'],
+            ['label' => 'التدفئة بالغاز', 'value' => 'gas'],
+            ['label' => 'التدفئة المركزية بالغلايات والمياه الساخنة', 'value' => 'boiler_hot_water'],
+            ['label' => 'التدفئة المركزية من خلال وحدة معالجة الهواء', 'value' => 'central_ahu'],
+        ];
+    }
+}
