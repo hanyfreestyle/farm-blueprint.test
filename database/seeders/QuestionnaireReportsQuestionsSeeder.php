88c1a1a5911df7a818e4b7cca00af03965cee36d
hanyfreestyle <81318840+hanyfreestyle@users.noreply.github.com>
2026-08-24 14:52:49 +0300

    Add reports question orchestrator

---
+++ database/seeders/QuestionnaireReportsQuestionsSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class QuestionnaireReportsQuestionsSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Question seeders will be added here when section 5 question design begins.
+    }
+}
