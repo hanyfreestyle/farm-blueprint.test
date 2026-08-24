ddbb5a1062fbc42dfe0270e2015723d23a571170
hanyfreestyle <81318840+hanyfreestyle@users.noreply.github.com>
2026-08-24 14:52:57 +0300

    Add settings question orchestrator

---
+++ database/seeders/QuestionnaireSettingsQuestionsSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class QuestionnaireSettingsQuestionsSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Question seeders will be added here when section 6 question design begins.
+    }
+}
