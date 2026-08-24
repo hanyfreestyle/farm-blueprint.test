3425af836a192f9a68d70b5db267184e7b2f1ccb
hanyfreestyle <81318840+hanyfreestyle@users.noreply.github.com>
2026-08-24 12:24:47 +0300

    Normalize questionnaire section order

--- database/seeders/QuestionnaireSectionSeeder.php
+++ database/seeders/QuestionnaireSectionSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\QuestionnaireSection;
 use Database\Seeders\Sections\QuestionnaireFarmStructureSectionSeeder;
 use Database\Seeders\Sections\QuestionnaireHerdSetupSectionSeeder;
 use Database\Seeders\Sections\QuestionnaireMasterDataSectionSeeder;
@@ -22,5 +23,21 @@ class QuestionnaireSectionSeeder extends Seeder
             QuestionnaireWorkflowSectionSeeder::class,
             QuestionnaireReportsSectionSeeder::class,
         ]);
+
+        $mainSectionOrder = [
+            'إدارة البيانات الأساسية' => 1,
+            'هيكل المزرعة' => 2,
+            'إعدادات التشغيل ودورة الإنتاج' => 3,
+            'تكوين وإدخال القطيع' => 4,
+            'الحركات ودورة التشغيل الفعلية' => 5,
+            'التقارير والإشعارات ومؤشرات الأداء' => 6,
+        ];
+
+        foreach ($mainSectionOrder as $name => $sortOrder) {
+            QuestionnaireSection::query()
+                ->whereNull('parent_id')
+                ->where('name', $name)
+                ->update(['sort_order' => $sortOrder]);
+        }
     }
 }
