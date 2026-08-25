205cf290356907a8cc3fc5880988294daa34d1b3
hanyfreestyle <81318840+hanyfreestyle@users.noreply.github.com>
2026-08-25 16:39:54 +0300

    Wire analytical entity pages questions seeder

--- database/seeders/QuestionnaireReportsQuestionsSeeder.php
+++ database/seeders/QuestionnaireReportsQuestionsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Database\Seeders\Questions\Reports\AnalyticalEntityPagesQuestionsSeeder;
 use Database\Seeders\Questions\Reports\BirthLactationWeaningReportsQuestionsSeeder;
 use Database\Seeders\Questions\Reports\DailyOperationsDashboardQuestionsSeeder;
 use Database\Seeders\Questions\Reports\FertilityMatingPregnancyReportsQuestionsSeeder;
@@ -34,6 +35,7 @@ class QuestionnaireReportsQuestionsSeeder extends Seeder
             HousingOccupancyCapacityOperationsReportsQuestionsSeeder::class,
             OperationalTaskPerformanceReportsQuestionsSeeder::class,
             TrendsComparisonsAnalysisQuestionsSeeder::class,
+            AnalyticalEntityPagesQuestionsSeeder::class,
         ]);
     }
 }
